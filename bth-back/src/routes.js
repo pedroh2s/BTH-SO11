23ba7bccd2f523c02e7c2a4333b976aedad16e83
pedrh2s <47404690+pedroh2s@users.noreply.github.com>
2020-04-07 10:26:19 -0300

    first commit

---
+++ bth-back/src/routes.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const {celebrate, Segments, Joi} = require('celebrate');
+
+const OngController = require('./Controller/OngController');
+const IncidentsController = require('./Controller/IncidentsController');
+const ProfileController = require('./Controller/ProfileController');
+const SessionController = require('./Controller/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    id:Joi.string().required(),
+  })
+}), SessionController.create);
+
+routes.get('/ongs', OngController.index);
+
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name:Joi.string().required(),
+    email:Joi.string().required().email(),
+    whatsapp:Joi.string().required().min(10).max(11),
+    city:Joi.string().required(),
+    uf:Joi.string().required().length(2),
+  })
+}), OngController.create);
+
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization:Joi.string().required(),
+  }).unknown(),
+}), ProfileController.index);
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number(),
+  })
+}), IncidentsController.index);
+
+routes.post('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization:Joi.string().required(),
+  }).unknown(),
+  [Segments.BODY]: Joi.object().keys({
+    title:Joi.string().required(),
+    description:Joi.string().required().email(),
+    value:Joi.number().required(),    
+  })
+}), IncidentsController.create);
+
+routes.delete('/incidents/:id', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization:Joi.string().required(),
+  }).unknown(),
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}), IncidentsController.destroy);
+
+module.exports = routes;
