bc6a267235d5878d8b9f5531597138f2112123c4
Pedro Henrique dos Santos Silva <pedroh@toledobrasil.com.br>
2020-04-08 12:57:25 -0300

    Start implementation

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function Details(){
+    return(
+        <View style={styles.container}>
+            
+        </View>
+    );
+
+}
